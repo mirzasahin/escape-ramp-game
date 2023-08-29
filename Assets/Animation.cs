ad3a12b26ba5a9d20198a93d59e903816928a872
Mirza Sahin <salihmirza123@gmail.com>
2023-08-29 08:52:45 +0300

    Add collectible items

---
+++ Assets/Animation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Animation : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DORotate(new Vector3(0, 360, 0), 5f)
+            .SetLoops(-1, LoopType.Restart)
+            .SetRelative()
+            .SetEase(Ease.Linear);
+
+        transform.DOMoveY(1.2f, 3f).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    
+
+
+}
