e47c17e6cd74a0b92381d3a96d02b16103d1427c
Mirza Sahin <salihmirza123@gmail.com>
2023-08-29 22:53:23 +0300

    Add FOV animation

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -1,4 +1,5 @@
-    using System.Collections;
+using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,16 @@ public class CameraFollower : MonoBehaviour
 {
     [SerializeField] GameObject target;
     Vector3 offset = new Vector3(5.5f, 5.5f, 6.64f);
+
+    private Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
+        mainCamera = Camera.main;
+        mainCamera.fieldOfView = 15;
+        transform.position = target.transform.position + offset;
+
+        AnimateFOV();
         
     }
 
@@ -17,5 +25,12 @@ public class CameraFollower : MonoBehaviour
     {
         //transform.position = target.transform.position + offset;
         transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.deltaTime * 5);
+    }
+
+    private void AnimateFOV()
+    {
+        mainCamera.DOFieldOfView(50, 3f)
+            .SetEase(Ease.InOutQuad)
+            .SetDelay(1.5f);
     }
 }
